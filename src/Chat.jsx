c1805d60634bd7142e37bfe706d83ff42fc17d8d
Omkar Vasekar <125749197+omkarvasekar@users.noreply.github.com>
2024-03-28 12:16:07 +0530

    Update Chat.jsx

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -19,7 +19,7 @@ export default function Chat() {
     connectToWs();
   }, [selectedUserId]);
   function connectToWs() {
-    const ws = new WebSocket('wss://weak-jade-stingray-tam.cyclic.app');
+    const ws = new WebSocket('wss://weak-jade-stingray-tam.cyclic.app/');
     setWs(ws);
     ws.addEventListener('message', handleMessage);
     ws.addEventListener('close', () => {
